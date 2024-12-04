f2307d305d48ef1cc44c529cab1e732570b50996
Richard-Raph <richardakpan77@gmail.com>
2024-12-04 18:30:44 +0100

    "Updated Window.css to change blur radius from 20px to 16px, and modified Dock.jsx to reorder and rephrase tooltips for dock icons."

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -10,9 +10,9 @@ import terminal from '../assets/images/terminal.webp';
 const icons = [
   { id: 'Home', imgSrc: home, tooltip: 'Home' },
   { id: 'About', imgSrc: about, tooltip: 'About Me' },
-  { id: 'Contact', imgSrc: contact, tooltip: 'Contact' },
   { id: 'Project', imgSrc: project, tooltip: 'Projects' },
   { id: 'Blog', imgSrc: blog, tooltip: 'Follow my trends' },
+  { id: 'Contact', imgSrc: contact, tooltip: 'Talk to me' },
   { id: 'Terminal', imgSrc: terminal, tooltip: 'Hire me!' },
 ];
 
