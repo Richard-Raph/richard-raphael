43fc99a5e62ead5ce23e7c49a308e9ba04e6dc0c
Richard-Raph <richardakpan77@gmail.com>
2024-12-06 17:25:47 +0100

    checking out something

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,13 @@
 export default function Home() {
   return (
     <section className='window-content'>
-      <div className='glass first'>View Source</div>
+      <div className='glass'>View Source</div>
 
       <div className='glass sec'>Watch Video</div>
 
       <div className='glass third'>Connect</div>
       
-      <div className='glass first'>View Source</div>
+      <div className='glass'>View Source</div>
 
       <div className='glass sec'>Watch Video</div>
 
