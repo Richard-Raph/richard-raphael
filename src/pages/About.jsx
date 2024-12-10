5a4b9224e497cad1e1e2d61109d345d45767c50f
Richard-Raph <richardakpan77@gmail.com>
2024-12-10 15:06:09 +0100

    `Refactor CSS grid to flexbox in Window.css and remove duplicate content in various pages`

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,44 +4,6 @@ export default function About() {
     return (
         <section className='window-content'>
             <div className='glass'>View Source</div>
-
-            <div className='glass'>Watch Video</div>
-
-            <div className='glass'>Connect</div>
-
-            <div className='glass'>View Source</div>
-
-            <div className='glass'>Watch Video</div>
-
-            <div className='glass'>Connect</div>
-
-            <div className='glass'>View Source</div>
-
-            <div className='glass'>Watch Video</div>
-
-            <div className='glass'>Connect</div>
-
-            <div className='glass'>Connect</div>
-
-            <div className='glass'>View Source</div>
-
-            <div className='glass'>Watch Video</div>
-
-            <div className='glass'>Connect</div>
-
-            <div className='glass'>View Source</div>
-
-            <div className='glass'>Watch Video</div>
-
-            <div className='glass'>Connect</div>
-
-            <div className='glass'>View Source</div>
-
-            <div className='glass'>Watch Video</div>
-
-            <div className='glass'>Connect</div>
-
-            <div className='glass'>Connect</div>
         </section>
     );
 }
