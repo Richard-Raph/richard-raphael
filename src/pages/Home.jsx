50033a1510ca6aa871c77790cbcaa919a6807a1c
Richard-Raph <richardakpan77@gmail.com>
2024-12-23 10:23:59 +0100

    added devcard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,45 +3,12 @@
 export default function Home() {
   return (
     <section className='window-content'>
-      <div className='glass'>View Source</div>
-
-      <div className='glass'>Watch Video</div>
-
-      <div className='glass'>Connect</div>
-      
-      <div className='glass'>View Source</div>
-
-      <div className='glass'>Watch Video</div>
-
-      <div className='glass'>Connect</div>
-      
-      <div className='glass'>View Source</div>
-
-      <div className='glass'>Watch Video</div>
-
-      <div className='glass'>Connect</div>
-      
-      <div className='glass'>Connect</div>
-      
-      <div className='glass'>View Source</div>
-
-      <div className='glass'>Watch Video</div>
-
-      <div className='glass'>Connect</div>
-      
-      <div className='glass'>View Source</div>
-
-      <div className='glass'>Watch Video</div>
-
-      <div className='glass'>Connect</div>
-      
-      <div className='glass'>View Source</div>
-
-      <div className='glass'>Watch Video</div>
-
-      <div className='glass'>Connect</div>
-      
-      <div className='glass'>Connect</div>
+      <a href='https://app.daily.dev/rich_tech123'>
+        <img src='https://api.daily.dev/devcards/v2/F5cxAxeNlHAqBVCt67SDW.png?type=wide&r=wg8' width='652' alt='Richard Raphael A.&#39;s Dev Card' />
+      </a>
+      <div className='glass'>
+        My name is Richard Raphael
+      </div>
     </section>
   );
 }
