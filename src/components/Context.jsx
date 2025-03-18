ad835f87b76398890c1e7280a0080e891dd385b6
Richard-Raph <richardakpan77@gmail.com>
2025-03-18 10:38:00 +0100

    `Refactor Context menu styles and content`

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -1,13 +1,37 @@
 import PropTypes from 'prop-types';
 import '../assets/css/Context.css';
+import {
+    FiCopy,
+    FiFolder,
+    FiGithub,
+    FiRefreshCcw,
+} from 'react-icons/fi';
 
 export default function Context({ x, y }) {
     return (
-        <ul className='custom-menu' style={{ top: `${y}px`, left: `${x}px` }}>
-            <li onClick={() => window.location.reload()}>Refresh</li>
-            <li onClick={() => alert('Action 2 triggered!')}>Action 2</li>
-            <li onClick={() => alert('Action 3 triggered!')}>Action 3</li>
-        </ul>
+        <div className='context-menu' style={{ top: `${y}px`, left: `${x}px` }}>
+            <div className='menu-section'>
+                <div className='menu-item' onClick={() => window.location.reload()}>
+                    <FiRefreshCcw size={20} color='rgba(0, 0, 0, 0.7)' />
+                    <span>Refresh Portfolio</span>
+                </div>
+                <div className='menu-item' onClick={() => window.open('https://github.com/Richard-Raph', '_blank')}>
+                    <FiGithub size={20} color='rgba(0, 0, 0, 0.7)' />
+                    <span>View GitHub</span>
+                </div>
+            </div>
+            <span />
+            <div className='menu-section'>
+                <div className='menu-item' onClick={() => window.open('/projects', '_blank')}>
+                    <FiFolder size={20} color='rgba(0, 0, 0, 0.7)' />
+                    <span>Open Project</span>
+                </div>
+                <div className='menu-item' onClick={() => navigator.clipboard.writeText(window.location.href)}>
+                    <FiCopy size={20} color='rgba(0, 0, 0, 0.7)' />
+                    <span>Copy URL</span>
+                </div>
+            </div>
+        </div>
     );
 }
 
